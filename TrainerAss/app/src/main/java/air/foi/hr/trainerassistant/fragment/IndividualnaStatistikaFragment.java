04040b6dbbe1f7d5c60ffda90e5f55dce185637a
atomas <atomas@foi.hr>
2016-08-06 13:29:45 +0200

    Priprema Fragmenta za individualnu statistiku
    Kreiranje kostura fragmenta, logika ulazi u sljedećim commitovima

---
+++ TrainerAss/app/src/main/java/air/foi/hr/trainerassistant/fragment/IndividualnaStatistikaFragment.java
@@ -0,0 +1,62 @@
+package air.foi.hr.trainerassistant.fragment;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import air.foi.hr.trainerassistant.HttpManager;
+import air.foi.hr.trainerassistant.Izbornik;
+import air.foi.hr.trainerassistant.JSONParser;
+import air.foi.hr.trainerassistant.R;
+import air.foi.hr.trainerassistant.RequestPackage;
+import air.foi.hr.trainerassistant.adapter.RezultatiAdapter;
+import air.foi.hr.trainerassistant.api.NavigationItem;
+import air.foi.hr.trainerassistant.base.BaseFragment;
+import air.foi.hr.trainerassistant.model.Atleticar;
+import air.foi.hr.trainerassistant.model.Disciplina;
+
+public class IndividualnaStatistikaFragment extends BaseFragment implements NavigationItem {
+
+    private int position;
+
+    public IndividualnaStatistikaFragment(){
+        //konstruktor
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_rezultati;
+    }
+
+    @Override
+    protected void init() {
+        Log.d("Inicijalizacija", "Inicijalizacija");
+        //inicijalizacija
+    }
+
+    @Override
+    public String getItemName() {
+        return "Individualna statistika";
+    }
+
+    @Override
+    public int getPosition() {
+        return position;
+    }
+
+    @Override
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    @Override
+    public Fragment getFragment() {
+        return this;
+    }
+}
